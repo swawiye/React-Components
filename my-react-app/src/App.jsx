6c9945e282718a3db3ff5796f00671a05d1e656f
swawiye <sovereign.wawiye@gmail.com>
2025-04-29 22:14:49 +0300

    setting up

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+    <h1>React Lesson One</h1>
+    <p>
+      React is a JavaScript library created by Facebook. 
+      It is a User Interface (UI) library and is used as 
+      a tool for building UI components.
+    </p>
+    </>
+  )
+}
+
+export default App
